8883b3f5a791ca00c24a4cddc51ac2eda2da202b
JJorgensen <jasoncjorgensen@gmail.com>
2021-03-22 23:37:27 -0600

    built out some of the views

--- server.js
+++ server.js
@@ -4,15 +4,26 @@ const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 
+
+
+
+
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
+//Set Handlbars
+const exphbs = require("express-handlebars");
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+ 
+
+
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
