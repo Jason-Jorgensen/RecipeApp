2b2ad4e3084fbc5dffce3e5da6be6c3c8fee8e4c
JJorgensen <jasoncjorgensen@gmail.com>
2021-03-25 22:58:23 -0600

    Toggled buttons working

--- public/js/recipes.js
+++ public/js/recipes.js
@@ -2,22 +2,26 @@ document.addEventListener('DOMContentLoaded',(event) => {
     if (event) {
         console.info('DOM loaded');
     };
-    const axios = require('axios');
+
+    $('.click-to-toggle').click(function(){
+        $(this).toggleClass('selected');
+     });
+
     const getRecipes = document.getElementById('recipeBtn');
 
     if (getRecipes) {
         getRecipes.addEventListener('click', (e) => {
             e.preventDefault();
             console.log('working');
-
-            axios.get('https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples&apiKey=51f3cdfc80964978a1b1035f9bf64575', {
-            }).then(function (response) {
-              console.log(response);
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-        })
+            fetch("/api/search/:ingredients", {
+                method:'POST',
+            }).then((res) => 
+        //     res.json(),
+        //     ).then((response) => 
+        //         console.log(response)
+        // );
+        console.log(res.hbsObject));
+        });
     };
 
 
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,20 +1,37 @@
 // Requiring path to so we can use relative routes to our HTML files
+const { reverse } = require("dns");
 const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  // app.get("/", (req, res) => {
-  //   // If the user already has an account send them to the members page
-  //   if (req.user) {
-  //     res.redirect("/members");
-  //   }
-  //   res.sendFile(path.join(__dirname, "../public/signup.html"));
-  // });
 
+
+  // const dairy = [
+  //   "milk","butter","cream"
+  // ];
+  const items = [
+    {
+      dairy:["milk","butter","cream","cheese"],
+    },
+    {
+      meat:["ground beef","turkey"],
+    },
+    {
+      grains:["flour", "bread", "tortilla"],
+    } 
+  ];
+  
+  // [{type:"milk"},{type:"butter"},{type:"eggs"}];
   app.get("/", (req,res) => {
-    res.render("index");
+    // res.render("index");
+    // res.render('index', { ingredients: dairy });
+    res.render('index', {
+      dairy: items[0],
+      meat: items[1],
+      grains: items[2],
+    });
   });
 
   app.get("/login", (req, res) => {
@@ -31,5 +48,7 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
-  
+  app.get("/search/:ingredients", (req,res) => {
+    res.render("recipes");
+  });
 };
